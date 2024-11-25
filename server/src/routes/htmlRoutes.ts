9de58d701b9b71506641cd68ba61d90493b600eb
CityWasTaken <smithfaheed@gmail.com>
2024-11-25 14:24:13 -0500

    Installed all default packages that were not installed

--- server/src/routes/htmlRoutes.ts
+++ server/src/routes/htmlRoutes.ts
@@ -1,10 +1,17 @@
-// import path from 'node:path';
-// import { fileURLToPath } from 'node:url';
-import { Router } from 'express';
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = path.dirname(__filename);
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { Response, Router } from 'express';
+
 const router = Router();
 
 // TODO: Define route to serve index.html
+if (process.env.PORT) {
+    router.get('*', (_, responseObj: Response) => {
+        const __filename = fileURLToPath(import.meta.url);
+        const __dirname = dirname(__filename);
+
+        responseObj.sendFile(path.join(__dirname, '../../../client/dist/index.html'));
+    });
+}
 
 export default router;
